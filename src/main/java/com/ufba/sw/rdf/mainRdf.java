af537cd5e5dd86d8c7fe01a2e214a32ca98cbb06
Brenno <brennodemello@hotmail.com>
2017-07-03 01:12:44 -0300

    Testes Comunicações

--- src/main/java/com/ufba/sw/rdf/mainRdf.java
+++ src/main/java/com/ufba/sw/rdf/mainRdf.java
@@ -8,7 +8,11 @@ package com.ufba.sw.rdf;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.vocabulary.VCARD;
 
 /**
@@ -20,7 +24,7 @@ public class mainRdf {
 
 
   public static void main(String [] args){
-
+    /*  
     // some definitions
     String personURI = "http://somewhere/JohnSmith";
     String fullName  = "John Smith";
@@ -33,6 +37,46 @@ public class mainRdf {
 
     // add the property
     johnSmith.addProperty(VCARD.FN, fullName); 
+    */
+            
+            
+    // some definitions
+    String personURI    = "http://somewhere/JohnSmith";
+    String givenName    = "John";
+    String familyName   = "Smith";
+    String fullName     = givenName + " " + familyName;
+
+    // create an empty Model
+    Model model = ModelFactory.createDefaultModel();
+
+    // create the resource
+    //   and add the properties cascading style
+    Resource johnSmith = model.createResource(personURI)
+         .addProperty(VCARD.FN, fullName).addProperty(VCARD.N, model.createResource().addProperty(VCARD.Given, givenName).addProperty(VCARD.Family, familyName));
+  
+    // list the statements in the Model
+      StmtIterator iter = model.listStatements();
+
+    // print out the predicate, subject and object of each statement
+    while (iter.hasNext()) {
+        Statement stmt      = iter.nextStatement();  // get next statement
+        Resource  subject   = stmt.getSubject();     // get the subject
+        Property  predicate = stmt.getPredicate();   // get the predicate
+        RDFNode   object    = stmt.getObject();      // get the object
+
+        System.out.print(subject.toString());
+        System.out.print(" " + predicate.toString() + " ");
+        if (object instanceof Resource) {
+           System.out.print(object.toString());
+        } else {
+            // object is a literal
+            System.out.print(" \"" + object.toString() + "\"");
+        }
+
+            System.out.println(" .");
+        } 
+
+    
   }
  
 }
